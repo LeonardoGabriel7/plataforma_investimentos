1521011e6e4d461ad4835ef15b112ec0bbb385bd
Leonardo Gabriel <l3onardo18072006@gmail.com>
2024-04-15 15:25:37 -0300

    investimentos

--- script.js
+++ script.js
@@ -2,18 +2,3 @@ document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('menu').classList.toggle('open-sidebar');
 });
 
-function active1(){
-    document.querySelector("#dash1").style.backgroundColor = "blue"
-}
-
-function active2(){
-    document.querySelector('#carteira').style.backgroundColor = "blue"
-}
-
-function active3(){
-    document.querySelector('#gasto').style.backgroundColor = "blue"
-}
-
-function active4(){
-    document.querySelector('#calculadora').style.backgroundColor = "blue"
-}
