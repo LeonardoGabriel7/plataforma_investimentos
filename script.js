caf1e7db992f093f6c3697f187427a80ffb45db4
Leonardo Gabriel <l3onardo18072006@gmail.com>
2024-04-15 15:20:34 -0300

    .

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('menu').classList.toggle('open-sidebar');
 });
 
+function active1(){
+    document.querySelector("#dash1").style.backgroundColor = "blue"
+}
 
-// sidebar = menu
+function active2(){
+    document.querySelector('#carteira').style.backgroundColor = "blue"
+}
+
+function active3(){
+    document.querySelector('#gasto').style.backgroundColor = "blue"
+}
+
+function active4(){
+    document.querySelector('#calculadora').style.backgroundColor = "blue"
+}
