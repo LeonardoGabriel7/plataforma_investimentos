e837dc38ba8847f2b2f1ec87178e058688b9f46c
Leonardo Gabriel <l3onardo18072006@gmail.com>
2024-04-15 21:03:16 -0300

    js

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('menu').classList.toggle('open-sidebar');
 });
 
+//INVESTIMENTOS
+
+//PEGAR VALOR DOS INPUTS:
+let rendaFixaValue = document.querySelector("#rendaFixa").value
+let acoesValue = document.querySelector("#Acoes").value
+let btcValue = document.querySelector("#btc").value
+let fundoImobiliarioValue = document.querySelector("#FundoImobiliario").value
+
+let totalValue = rendaFixaValue + acoesValue + btcValue + fundoImobiliarioValue ;
+
+    let rendaFixaPorcentagem = (rendaFixaValue / totalValue) * 100;
+    let acoesPorcentagem = (acoesValue / totalValue) * 100;
+    let btcPorcentagem = (btcValue / totalValue) * 100;
+    let fundoImobiliarioPorcentagem = (fundoImobiliarioValue / totalValue) * 100;
+
+    
