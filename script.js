b4491c8d38cc1b7279da2dd5939bfd35370e75bf
Leonardo Gabriel <l3onardo18072006@gmail.com>
2024-04-18 20:22:15 -0300

    calculadora

--- script.js
+++ script.js
@@ -55,4 +55,15 @@ function gerarGrafico() {
             });
         }
 
+
   
+        // CALCULADORA DE JUROS COMPOSTOS
+
+   document.getElementById("btnCalcular").addEventListener('click', function(){
+        const  value = document.getElementById('value').value;
+        const  taxa = document.getElementById('taxa').value / 100;
+        const  tempo = document.getElementById('tempo').value;
+    
+        const total = value * (1 + taxa)**tempo;
+        document.getElementById("total").innerHTML = ("R$" + total.toFixed(2)).replace('.', ",");
+    })    
